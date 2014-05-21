c8f80d56a692226ca2fc9290b77ec5c85f5e5a74
Memfis <badalloffsh@gmail.com>
2014-05-21 17:24:39 +0500

    Cart Created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 class Product < ActiveRecord::Base
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :presence => true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -11,6 +15,17 @@ class Product < ActiveRecord::Base
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+  #Убеждаемся в отсутствии покупок ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существует товарная позиция (покупка)')
+      return false
+    end
   end
 
 end
